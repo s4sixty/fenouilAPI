5ce0de85ff9ffa68decd30def4ed578ddc433122
Samir AMARA <samirq1@outlook.com>
2019-03-12 22:56:24 +0100

    Authentification

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+ 
 class User extends Authenticatable
 {
     use HasApiTokens,Notifiable;
